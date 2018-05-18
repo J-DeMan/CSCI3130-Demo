be9045ee552932a3ed581f65bc98ad57f060d8f6
Jerrett DeMan <jerrett.deman@gmail.com>
2018-05-18 12:26:23 -0300

    Adds UI component event handlers.

--- app/src/main/java/com/example/acme/csci3130_demo/MainActivity.java
+++ app/src/main/java/com/example/acme/csci3130_demo/MainActivity.java
@@ -2,6 +2,8 @@ package com.example.acme.csci3130_demo;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +11,12 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    // Changes the output view text to the text currently in the input view
+    public void apply(View v) {
+        TextView inputField = findViewById(R.id.inputView);
+        TextView outputField = findViewById(R.id.outputView);
+        outputField.setText(inputField.getText());
     }
 }
